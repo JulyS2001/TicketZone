ad38384bc9edabba716b7d47dcc71497fcf9d161
Julian Sorli <114437137+JulyS2001@users.noreply.github.com>
2025-05-30 13:24:44 -0300

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -3,8 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'smtp.gmail.com';
 $config['smtp_port'] = 587;
-$config['smtp_user'] = 'rukysorli@gmail.com'; // dirección de correo
-$config['smtp_pass'] = 'JulianSorli1'; // contraseña de correo
+$config['smtp_user'] = ''; // dirección de correo
+$config['smtp_pass'] = ''; // contraseña de correo
 $config['mailtype'] = 'html'; 
 $config['charset'] = 'utf8mb4';
 $config['newline'] = "\r\n"; 
